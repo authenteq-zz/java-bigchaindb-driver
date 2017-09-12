99e70f6ca2c1342f6e9e9614c362ae51971fae7b
Bohdan Bezpartochnyy <bohdan@authenteq.com>
2017-09-12 17:22:31 +0300

    Added license header

--- src/test/java/com/authenteq/BigchaindbTransactionTest.java
+++ src/test/java/com/authenteq/BigchaindbTransactionTest.java
@@ -1,3 +1,22 @@
+/*
+ * (C) Copyright 2017 Authenteq (https://authenteq.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     Bohdan Bezpartochnyi <bohdan@authenteq.com>
+ */
+
 package com.authenteq;
 
 import net.i2p.crypto.eddsa.EdDSAEngine;
